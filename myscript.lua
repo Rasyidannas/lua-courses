9b7b9c98648aa578c6a13562e6e37718e07f1e77
rasyidannas <rasshit.dsgn@gmail.com>
2025-09-05 02:01:35 +0000

    10.Variables.mp4

--- myscript.lua
+++ myscript.lua
@@ -1,2 +1,26 @@
 print("Hello World!")
 print(12 * 45)
+
+-- Variables
+score = 100
+print(score)
+print(type(score))
+
+salary = 190.9
+print(salary)
+
+name = "Rasyid"
+print(type(name))
+
+
+print(age) -- it will return nil
+
+-- Data type in lua
+  -- nil
+  -- number
+  -- string
+    greeting = "Good morning"
+    -- Concatination
+    print(greeting.." "..name)
+  -- function
+  -- table
