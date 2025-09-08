cf74f7f10cbfcb74496728739ed123a396ebe2e7
rasyidannas <rasshit.dsgn@gmail.com>
2025-09-08 01:39:03 +0000

    15. Condition Statement.mp4

--- myscript.lua
+++ myscript.lua
@@ -32,14 +32,25 @@ print(age) -- it will return nil
 
 -- Increament and Decreament
 level = 1
-score = 0
+score = 10
 num_lives = 5
+time_ellapsed = 0
 
 -- incrementing level and Lua don't have assigment operator like JS or PHP
 level = level + 1
 
 num_live = num_lives - 1
 
+-- Conditional
+-- if
+if score >= 1000 then
+  level = level + 1
+  num_lives = 5
+else
+  time_ellapsed = time_ellapsed + 1
+end
 
-
-
+print("level: " .. level)
+print("score: " .. score)
+print("lives: " .. num_lives)
+print("time: " .. time_ellapsed)
